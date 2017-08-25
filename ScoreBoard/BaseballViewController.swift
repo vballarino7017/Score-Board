d69b93e483cdf39ba0e60cc1c7058d78d85affc6
Daniella Ballarino <daniellaballarino@daniellas-macbook-pro.local>
2017-08-25 00:13:49 -0500

    worked on sports

---
+++ ScoreBoard/BaseballViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BaseballViewController.swift
+//  ScoreBoard
+//
+//  Created by Daniella Ballarino on 8/25/17.
+//  Copyright © 2017 Daniella Ballarino. All rights reserved.
+//
+
+import UIKit
+
+class BaseballViewController: UIViewController {
+
+    @IBOutlet weak var thirdbase: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    @IBAction func Third(_ sender: UIButton) {
+        thirdbase.backgroundColor = UIColor.yellow
+    }
+    @IBAction func Second(_ sender: UIButton) {
+    }
+    @IBAction func First(_ sender: UIButton) {
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+ }
---
+++ ScoreBoard/FootballViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FootballViewController.swift
+//  ScoreBoard
+//
+//  Created by Daniella Ballarino on 8/24/17.
+//  Copyright © 2017 Daniella Ballarino. All rights reserved.
+//
+
+import UIKit
+
+class FootballViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
